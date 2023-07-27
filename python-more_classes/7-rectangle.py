7a28c7ef12b32958b56f9f1302a4750afd947eed
OLUWATIDEMILADE <o.olorunsol@alustudent.com>
2023-07-27 13:13:06 -0700

    7

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -8,10 +8,12 @@ This module provides a Rectangle class.
 class Rectangle:
     """A Rectangle class with attributes width and height,
     methods area, perimeter, print, str, repr, and del, and
-    class attribute number_of_instances that keeps track of # of instances.
+    class attribute number_of_instances that keeps track of # of instances,
+    and class attribute print_symbol which is used as symbol for printing.
     """
 
     number_of_instances = 0
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         self.width = width
@@ -48,7 +50,11 @@ class Rectangle:
     def __str__(self):
         total = ""
         for i in range(self.__height):
-            total += ("#" * self.__width)
+            for j in range(self.__width):
+                try:
+                    total += str(self.print_symbol)
+                except Exception:
+                    total += type(self).print_symbol
             if i is not self.__height - 1:
                 total += "\n"
         return total
